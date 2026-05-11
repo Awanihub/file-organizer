5d78dce1610097ba93f403078661933450e40d9d
Awantika <awantikasingh78@gmail.com>
2026-05-11 07:23:54 +0530

    feat: add starter index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * file-organizer
+ * A tool that automatically organizes files by type and date
+ */
+
+'use strict';
+
+const args = process.argv.slice(2);
+
+function main() {
+  console.log('Project: file-organizer');
+  console.log('Status: ready');
+  console.log('Args:', args);
+}
+
+main();
